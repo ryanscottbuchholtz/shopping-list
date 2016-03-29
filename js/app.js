8a643ddd17c78f07f300bafa4c310b3c1be1cca7
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-03-29 14:35:21 -0700

    validate user input

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 // Element Variables
 
 var newItem = document.getElementById('new_item');
-var addButton = document.getElementById('submit-button');
-var shoppingList = document.getElementById('shopping-list');
-var deletedShoppingList = document.getElementById('deleted-shopping-list');
-var savedShoppingList = document.getElementById('saved-shopping-list');
-var completeShoppingList = document.getElementById('complete-shopping-list');
+
+// Buttons
 var homeButton = document.getElementById('home');
 var deletedShoppingListButton = document.getElementById('deleted');
 var savedShoppingListButton = document.getElementById('delayed');
 var completedShoppingListButton = document.getElementById('complete');
+var addButton = document.getElementById('submit-button');
+// var deletedShoppingListButton = document.getElementById('deleted');
+// var savedShoppingListButton = document.getElementById('delayed');
+// var completedShoppingListButton = document.getElementById('complete');
+
+// Lists
+var allLists = document.getElementsByClassName('shop-list');
+var shoppingList = document.getElementById('shopping-list');
+var deletedShoppingList = document.getElementById('deleted-shopping-list');
+var savedShoppingList = document.getElementById('saved-shopping-list');
+var completeShoppingList = document.getElementById('complete-shopping-list');
+
+// Headers
+var allHeaders = document.getElementsByClassName('list-head');
 var shoppingListHeader = document.getElementById('shop-head');
 var deletedItemsHeader = document.getElementById('delete-head');
 var savedItemsHeader = document.getElementById('save-head');
-var completedItemsHeader = document. getElementById('bought-head');
+var completedItemsHeader = document.getElementById('bought-head');
 
 // Add item to list
 
@@ -56,10 +67,26 @@ var createNewShoppingListItem = function(newItemUserInput) {
 
 };
 
+function validateInput() {
+  if (newItem.value === '') {
+    alert("New item must be entered");
+    return false;
+  }
+  
+}
+
 var addItem = function () {
+
+  if (newItem.value === "") {
+    alert("New item must be entered");
+    return false;
+  }
+
   var listItem = createNewShoppingListItem(newItem.value);
   shoppingList.appendChild(listItem);
   
+  // store the new item in shopping list session storage
+
   newItem.value = "";
 
   buttonCheckboxFunc(listItem);
@@ -67,8 +94,8 @@ var addItem = function () {
 
 addButton.addEventListener("click", addItem);
 
-newItem.onkeydown = function(){
-    if(window.event.keyCode === 13){
+newItem.onkeyup = function(){
+    if(window.event.keyCode === 13){    //remove return keycode and write different code to accept return/submit
         addItem();
     }
 };
@@ -90,8 +117,9 @@ var buttonCheckboxFunc = function(groceryListItem) {
 var deleteGroceryItem = function () {
   listItem = this.parentNode.parentNode;
   parent = this.parentNode.parentNode.parentNode;
-  
   deletedShoppingList.appendChild(listItem);
+  // remove item from shopping list session storage
+  // add item to deleted item session storage
   
   parent.removeChild(listItem);
 };
@@ -100,6 +128,8 @@ var saveGroceryItem = function () {
   listItem = this.parentNode.parentNode;
   parent = this.parentNode.parentNode.parentNode;
   savedShoppingList.appendChild(listItem);
+  // remove item from current list session storage
+  // add item to saved item session storage
   parent.removeChild(listItem);
 };
 
@@ -107,68 +137,48 @@ var completeGroceryItem = function () {
   listItem = this.parentNode;
   parent = listItem.parentNode;
   completeShoppingList.appendChild(listItem);
+  // remove item from current list session storage
+  // add item to complete item session storage
   parent.removeChild(listItem);
 };
 
 var showShoppingList = function () {
-  shoppingList.style.display = "block";
-  shoppingListHeader.style.display ="inline";
-  deletedItemsHeader.style.display ="none";
-  savedItemsHeader.style.display = "none";
-  completedItemsHeader.style.display = "none";
-  savedShoppingList.style.display = "none";
-  completeShoppingList.style.display = "none";
-  deletedShoppingList.style.display = "none";
+  showThisListAndHeader(shoppingList, shoppingListHeader);
 };
 
 var showDeletedList = function () {
-  deletedShoppingList.style.display = "block";
-  shoppingListHeader.style.display = "none";
-  deletedItemsHeader.style.display = "inline";
-  savedItemsHeader.style.display = "none";
-  completedItemsHeader.style.display = "none";
-  shoppingList.style.display = "none";
-  savedShoppingList.style.display = "none";
-  completeShoppingList.style.display = "none";
+  showThisListAndHeader(deletedShoppingList, deletedItemsHeader);
  };
 
 var showDelayedList = function () {
-  savedShoppingList.style.display = "block";
-  shoppingListHeader.style.display = "none";
-  deletedItemsHeader.style.display = "none";
-  savedItemsHeader.style.display = "inline";
-  completedItemsHeader.style.display = "none";
-  shoppingList.style.display = "none";
-  completeShoppingList.style.display = "none";
-  deletedShoppingList.style.display = "none";
+  showThisListAndHeader(savedShoppingList, savedItemsHeader);
 };
 
 var showCompletedList = function () {
-  completeShoppingList.style.display = "block";
-  shoppingList.style.display = "none";
-  deletedItemsHeader.style.display = "none";
-  savedItemsHeader.style.display = "none";
-  completedItemsHeader.style.display = "inline";
-  deletedShoppingList.style.display = "none";
-  savedShoppingList.style.display = "none";
-  shoppingListHeader.style.display = "none";
+  showThisListAndHeader(completeShoppingList, completedItemsHeader);
 };
+
+// Show Hide List/Header Function
+
+function showThisListAndHeader(listToShow, headerToShow) {
+  var showList = listToShow;
+  var showHead = headerToShow;
+
+    for (i = 0; i < allLists.length; i++) {
+      console.log(allLists[i]);
+      allLists[i].style.display = "none";
+    }
+
+    for (x = 0; x < allHeaders.length; x++) {
+        allHeaders[x].style.display = "none";
+      }
+ 
+  showList.style.display = "block";
+  showHead.style.display = "inline";
+  
+}
 
 homeButton.addEventListener('click', showShoppingList);
 deletedShoppingListButton.addEventListener('click', showDeletedList);
 savedShoppingListButton.addEventListener('click', showDelayedList);
 completedShoppingListButton.addEventListener('click', showCompletedList);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
